4c1d0e276ba4ae60992464f5308f0cc28ac2de31
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-04-08 08:13:43 +0300

    authorization is added

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../shared/services/auth.service";
+import { Subscription } from "rxjs";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
   form!: FormGroup;
+  aSub!: Subscription
 
-  constructor() {
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -18,10 +24,38 @@ export class LoginPageComponent implements OnInit {
       password: new FormControl(null, [Validators.required, Validators.minLength(6)])
     })
 
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        //Now you can enter to system with your data
+      } else if (params['accessDenied']) {
+        // Please login to system
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
   }
 
   onSubmit() {
+    // const user = {
+    //   email: this.form.value.email,
+    //   password: this.form.value.password
+    // }
+    // this.auth.login(user)
 
+    this.form.disable()
+
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      // () => console.log('Login success'),
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
   }
 
 }
