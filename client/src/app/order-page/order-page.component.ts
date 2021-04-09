eccabc79d54de88f22874fce48ed34023a2e678d
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-04-09 10:55:18 +0300

    order structure and markup

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
+import { MaterialInstance, MaterialService } from "../shared/classes/material.service";
+import { OrderService } from "./order.service";
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.scss']
+  styleUrls: ['./order-page.component.scss'],
+  providers: [OrderService]
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('modal') modalRef!: ElementRef
+  isRoot!: boolean
+  modal!: MaterialInstance
 
-  constructor() { }
+  constructor(private router: Router,
+              private order: OrderService) { }
 
   ngOnInit(): void {
+    this.isRoot = this.router.url === '/order'
+    // отслеживаем смену роута не только во время инициализации страницы, а все время:
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+      console.log('Event', event)
+        this.isRoot = this.router.url === '/order'
+      }
+    })
   }
 
+  ngOnDestroy() {
+    this.modal.destroy()
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialService.initModal(this.modalRef)
+  }
+
+  open() {
+    this.modal.open()
+  }
+
+  cancel() {
+    this.modal.close()
+  }
+
+  submit() {
+    this.modal.close()
+  }
 }
