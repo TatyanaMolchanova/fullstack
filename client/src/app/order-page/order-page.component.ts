d5adc077883e686f7d1e175eddb76357d7aaf99d
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-04-09 14:08:56 +0300

    order is ready

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -2,7 +2,9 @@ import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } fr
 import { NavigationEnd, Router } from "@angular/router";
 import { MaterialInstance, MaterialService } from "../shared/classes/material.service";
 import { OrderService } from "./order.service";
-import { OrderPosition } from "../shared/interfaces";
+import {Order, OrderPosition} from "../shared/interfaces";
+import {OrdersService} from "../shared/services/orders.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-order-page',
@@ -14,16 +16,19 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('modal') modalRef!: ElementRef
   isRoot!: boolean
   modal!: MaterialInstance
+  pending: boolean = false
+  onSub!: Subscription
 
   constructor(private router: Router,
-              public order: OrderService) { }
+              public order: OrderService,
+              private ordersService: OrdersService) { }
 
   ngOnInit(): void {
     this.isRoot = this.router.url === '/order'
     // отслеживаем смену роута не только во время инициализации страницы, а все время:
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-      console.log('Event', event)
+      // console.log('Event', event)
         this.isRoot = this.router.url === '/order'
       }
     })
@@ -31,6 +36,10 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
   ngOnDestroy() {
     this.modal.destroy()
+
+    if (this.onSub) {
+      this.onSub.unsubscribe()
+    }
   }
 
   ngAfterViewInit() {
@@ -50,6 +59,32 @@ export class OrderPageComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   submit() {
+    // сейчас будем работать с сервером: this.pending = true
+    this.pending = true
+
     this.modal.close()
+
+    // const order: Order = this.order.list
+
+    const order: Order = {
+      // удаляем id из заказа для бэкенда и оставляем на UI
+      list: this.order.list.map(item => {
+        delete item._id
+        return item
+      })
+    }
+
+    this.onSub = this.ordersService.create(order).subscribe(
+      newOrder => {
+        MaterialService.toast(`Заказ №${newOrder.order} был добавлен.`)
+        this.order.clear()
+      },
+      error => MaterialService.toast(error.error.message),
+      () => {
+        this.modal.close()
+        //асинхронный код отработал
+        this.pending = false
+      }
+    )
   }
 }
