5e2eb688effcca5abfd50823f584dc59eed9cc36
tetiana.molchanova-at-022587608743 <tetiana.molchanova@edetek.com>
2021-04-09 20:06:21 +0300

    history page is ready

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
-import {OrdersService} from "../shared/services/orders.service";
-import {Subscription} from "rxjs";
-import {Order} from "../shared/interfaces";
+import { MaterialInstance, MaterialService } from "../shared/classes/material.service";
+import { OrdersService } from "../shared/services/orders.service";
+import { Subscription } from "rxjs";
+import { Filter, Order } from "../shared/interfaces";
 
 const STEP = 2
 
@@ -22,6 +22,7 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
   loading: boolean = false
   reloading: boolean = false
   noMoreOrders: boolean = false
+  filter: Filter = {}
 
   constructor(private ordersService: OrdersService) { }
 
@@ -31,10 +32,17 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   private fetch() {
-    const params = {
-      offset: this.offset,
-      limit: this.limit
-    }
+    // const params = {
+    //   offset: this.offset,
+    //   limit: this.limit
+    // }
+
+    const params = Object.assign({}, this.filter, {
+        offset: this.offset,
+        limit: this.limit
+    })
+
+    //Фильтрация, реализуем так фильтры!
     this.onSub = this.ordersService.fetch(params).subscribe(orders => {
       // добавляем предыдущие элементы при клике на Загрузить еще к новым - пагинация на одной странице, доподзагрузка
       this.orders = this.orders.concat(orders)
@@ -49,6 +57,19 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
     this.offset += STEP
     this.loading = true
     this.fetch()
+  }
+
+  applyFilter(filter: Filter) {
+    // debugger
+    this.orders = []
+    this.offset = 0
+    this.filter = filter
+    this.reloading = true
+    this.fetch()
+  }
+
+  isFiltered(): boolean {
+    return Object.keys(this.filter).length !== 0
   }
 
   ngAfterViewInit() {
